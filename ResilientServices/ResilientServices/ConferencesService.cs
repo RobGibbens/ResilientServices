a25773b445bc748e9ae0a366d2436665b3cf0159
Rob Gibbens <RobGibbens@gmail.com>
2014-12-23 16:29:22 -0500

    Wired up Connectivity

--- ResilientServices/ResilientServices/ConferencesService.cs
+++ ResilientServices/ResilientServices/ConferencesService.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Akavache;
+using Connectivity.Plugin;
 using Polly;
 using TekConf.Mobile.Core.Dtos;
 
 namespace ResilientServices
 {
-	public class ConferencesService : IConferencesService
-	{
-		private readonly IApiService _apiService;
-	    private const int MAX_RETRIES = 10;
+    public class ConferencesService : IConferencesService
+    {
+        private readonly IApiService _apiService;
 
-		public ConferencesService(IApiService apiService)
-		{
-			_apiService = apiService;
-		}
+        public ConferencesService(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
 
-		public async Task<List<ConferenceDto>> GetConferences()
-		{
-            var cache = BlobCache.LocalMachine;
-            var cachedConferences = cache.GetAndFetchLatest("conferences", GetRemoteConferences, offset =>
+        public async Task<List<ConferenceDto>> GetConferences()
+        {
+            var cachedConferences = BlobCache.LocalMachine.GetAndFetchLatest("conferences", GetRemoteConferences, offset =>
             {
                 TimeSpan elapsed = DateTimeOffset.Now - offset;
                 return elapsed > new TimeSpan(hours: 0, minutes: 10, seconds: 0);
@@ -29,13 +28,12 @@ namespace ResilientServices
 
             var conferences = await cachedConferences.FirstOrDefaultAsync();
 
-		    return conferences;
-		}
+            return conferences;
+        }
 
         public async Task<ConferenceDto> GetConference(string slug)
         {
-            var cache = BlobCache.LocalMachine;
-            var cachedConference = cache.GetAndFetchLatest(slug, () => GetRemoteConference(slug), offset =>
+            var cachedConference = BlobCache.LocalMachine.GetAndFetchLatest(slug, () => GetRemoteConference(slug), offset =>
             {
                 TimeSpan elapsed = DateTimeOffset.Now - offset;
                 return elapsed > new TimeSpan(hours: 0, minutes: 10, seconds: 0);
@@ -44,29 +42,36 @@ namespace ResilientServices
             var conference = await cachedConference.FirstOrDefaultAsync();
 
             return conference;
-
         }
 
 
-	    private async Task<List<ConferenceDto>> GetRemoteConferences()
-	    {
-            var conferences = await Policy
-                .Handle<Exception>()
-                .RetryAsync(MAX_RETRIES)
-                .ExecuteAsync(async () => await _apiService.UserInitiated.GetConferences());
-
+        private async Task<List<ConferenceDto>> GetRemoteConferences()
+        {
+            List<ConferenceDto> conferences = null;
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                conferences = await Policy
+                    .Handle<Exception>()
+                    .RetryAsync(retryCount: 5)
+                    .ExecuteAsync(async () => await _apiService.UserInitiated.GetConferences());
+            }
             return conferences;
-	    }
+        }
 
-		public async Task<ConferenceDto> GetRemoteConference(string slug)
-		{
-			var conference = await Policy
-					.Handle<Exception>()
-                    .RetryAsync(MAX_RETRIES)
-					.ExecuteAsync(async () => await _apiService.UserInitiated.GetConference(slug));
+        public async Task<ConferenceDto> GetRemoteConference(string slug)
+        {
+            ConferenceDto conference = null;
 
-			return conference;
-		}
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                conference = await Policy
+                    .Handle<Exception>()
+                    .RetryAsync(retryCount: 5)
+                    .ExecuteAsync(async () => await _apiService.UserInitiated.GetConference(slug));
+            }
 
-	}
+            return conference;
+        }
+
+    }
 }
