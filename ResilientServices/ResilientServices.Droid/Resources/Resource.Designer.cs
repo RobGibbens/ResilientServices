75eabe40e4466bbd3a53f4165257b279727eb6b5
Rob Gibbens <RobGibbens@arteksoftware.com>
2014-12-22 22:12:56 -0500

    Showing name

--- ResilientServices/ResilientServices.Droid/Resources/Resource.Designer.cs
+++ ResilientServices/ResilientServices.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace ResilientServices.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Splat.Resource.String.library_name = global::ResilientServices.Droid.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
@@ -53,6 +54,22 @@ namespace ResilientServices.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int library_name = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
 			{
 			}
 		}
